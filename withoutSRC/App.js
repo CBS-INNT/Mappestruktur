da9b71a374860b6931bbd4eeaa498a6bc43d9631
IdaHustedDavidsen <id.digi@cbs.dk>
2025-09-29 14:29:41 +0200

    flyttet det til mappen

---
+++ withoutSRC/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import AppNavigation from "./navigation/AppNavigation";
+import Colors from "./styles/GlobalStyles";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={styles.safeArea}
+        edges={["top", "left", "right", "bottom"]}
+      >
+        <AppNavigation />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+});
